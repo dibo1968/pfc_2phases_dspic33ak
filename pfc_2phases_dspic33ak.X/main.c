3dc37a292ada87962160f05a0f7c1bc1e653721a
dinub <dinub@Dinu-PC.local>
2025-07-09 11:15:03 +0300

    first

---
+++ pfc_2phases_dspic33ak.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: dinub
+ *
+ * Created on July 9, 2025, 11:09 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(void) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ pfc_2phases_dspic33ak.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=pfc_2phases_dspic33ak.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/pfc_2phases_dspic33ak.X.production.hex
